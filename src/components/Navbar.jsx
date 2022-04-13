b7688b7297907f26d919792eaa919e60118a825c
iamzhanaidarmukanov <iamzhanaidar.mukanov@gmail.com>
2022-04-13 12:38:59 +0600

    Initial Navbar Setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import {Button, Menu, Typography, Avatar} from 'antd'
 import {Link} from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, FundOutlined, MenuOutlined, BulbOutlined } from '@ant-design/icons'
+import icon  from '../images/cryptocurrency.png'
 
 const Navbar = () => {
   return (
     <div className='navbar-container'>
         <div className='logo-container'>
-            <Avatar />
+            <Avatar src={icon} size='large' />
             <Typography.Title level={2} className="logo">
                 <Link to='/'>Cryptoverse</Link>
             </Typography.Title>
